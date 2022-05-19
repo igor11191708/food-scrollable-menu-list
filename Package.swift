22b1470706d16b8b4865e06d76a10cca41181704
Igor <igorshelopaev@gmail.com>
2022-05-19 17:16:25 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/The-Igor/d3-color.git" , from: "1.0.2"),
-        .package(url: "https://github.com/The-Igor/d3-menu-bar.git" , from: "1.0.3"),
+        .package(url: "https://github.com/The-Igor/d3-menu-bar.git" , from: "1.0.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
